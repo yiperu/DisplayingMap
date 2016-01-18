6ce6fa025e3cb17811a88111ab540a2e11a899e9
Henry AT <yiperu@gmail.com>
2016-01-18 09:12:51 -0300

    Add some comentary

--- DisplayingMap/ViewController.m
+++ DisplayingMap/ViewController.m
@@ -24,6 +24,27 @@
   // Do any additional setup after loading the view, typically from a nib.
   [map01 setDelegate:self];
   [map01 setShowsUserLocation:YES];
+  
+  /*
+   For have in consideration:
+   if CLLocationManager.locationServicesEnabled() {
+   locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+   locationManager.requestLocation()
+   }
+   
+   
+   
+   That’s because there’s one more thing left to take care of. You need to provide the user with a reason for your request.
+   Open Supporting Files > info.plist and follow these steps:
+   Add “NSLocationWhenInUseUsageDescription” as a Key in the Information Property List.
+   Keep the Type as String.
+   Set the Value with the message to show the user, explaining why you’re asking for their location:
+   “Allow us to access your current location so we can autofill your start/end point.”
+   
+   
+   Note: NSLocationWhenInUseUsageDescription | .requestWhenInUseAuthorization() lets the app access the user’s location while the app is being used.
+   NSLocationAlwaysUsageDescription | .requestAlwaysAuthorization() lets the app access the user’s location, even while the app is backgrounded.
+   */
 
 //  [map01 ];
 
